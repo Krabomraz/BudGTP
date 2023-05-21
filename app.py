a3a13d200b9ce34ee62d93e340c4bf122bdd719d
Eugene Ovchinnikov <ov.zhen@gmail.com>
2023-05-21 12:06:03 +0700

    Migrations Done

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
+from dotenv import load_dotenv
+
 import telegram
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
+from flask_migrate import Migrate
 
 load_dotenv()
 
+# App and DB init
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://#{os.getenv('DB_USER')}:#{os.getenv('DB_PASSWORD')}@#{os.getenv('DB_HOST')}/#{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import *
+
+# TG Bot init
 bot = telegram.Bot(token=os.getenv('TG_BOT_API_TOKEN'))
 
 
@@ -31,4 +40,6 @@ def process_update(update):
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run()
